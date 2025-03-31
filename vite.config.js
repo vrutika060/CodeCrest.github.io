fcc9aa27ca34e81fa38845dbc7af34c9567922d4
my name is vrutika <birje.vrutika22@gmail.com>
2025-03-31 20:52:13 +0530

    Rebuilding frontend for Railway deployment

--- vite.config.js
+++ vite.config.js
@@ -16,11 +16,24 @@
 //   base: '/CodeCrest.github.io/',
 // });
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+// code before deploying on railway
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// base: '/CodeCrest.github.io/', 
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-base: '/CodeCrest.github.io/', 
+  base: '/', // Fix this line
 });
+
+
